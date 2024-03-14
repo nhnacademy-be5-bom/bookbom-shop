fca0a28d584dcb2d8d81594a0b97aeaf6b8ed1c5
youaresogay22 <youaresogayee@gmail.com>
2024-03-14 15:28:08 +0900

    hello spring

---
+++ src/main/java/shop/bookbom/shop/index/IndexController.java
@@ -0,0 +1,12 @@
+package shop.bookbom.shop.index;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    @GetMapping(value = {"/index.html","/"})
+    public String index(){
+        return "index/index";
+    }
+}
