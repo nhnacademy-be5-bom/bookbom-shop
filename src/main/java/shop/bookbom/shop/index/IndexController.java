b59222ced203b632df67dd79cf86dde430e83d14
youaresogay22 <youaresogayee@gmail.com>
2024-03-28 17:49:38 +0900

    [Feature] 상용환경에서 유레카 접속

--- src/main/java/shop/bookbom/shop/index/IndexController.java
+++ src/main/java/shop/bookbom/shop/index/IndexController.java
@@ -2,11 +2,13 @@ package shop.bookbom.shop.index;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/shop")
 public class IndexController {
-    @GetMapping(value = {"/index.html","/"})
-    public String index(){
+    @GetMapping(value = {"/index.html", "/"})
+    public String index() {
         return "index/index";
     }
 }
