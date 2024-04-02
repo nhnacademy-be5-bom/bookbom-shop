0cd789fe56a153764b7982332dba8fd508521e0a
hyeond0 <a98792530@gmail.com>
2024-04-02 11:31:56 +0900

    [feat] 공통 응답 및 예외처리

---
+++ src/main/java/shop/bookbom/shop/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,14 @@
+package shop.bookbom.shop.common.exception;
+
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import shop.bookbom.shop.common.CommonResponse;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BaseException.class)
+    public CommonResponse<Void> handleBaseException(BaseException e) {
+        return CommonResponse.fail(e.getErrorCode());
+    }
+}
