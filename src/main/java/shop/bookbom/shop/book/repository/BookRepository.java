6fb4d0cd83c1f7eced0b722a051aaa0dd1cc86b2
dudxor4587 <dudxor4587@gmail.com>
2024-04-03 10:39:45 +0900

    [fix] 파일 위치 및 500에러 수정

--- src/main/java/shop/bookbom/shop/book/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package shop.bookbom.shop.book.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import shop.bookbom.shop.book.entity.Book;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
--- src/main/java/shop/bookbom/shop/domain/booktag/repository/BookTagRepository.java
+++ src/main/java/shop/bookbom/shop/domain/booktag/repository/BookTagRepository.java
@@ -1,8 +1,9 @@
-package shop.bookbom.shop.booktag.repository;
+package shop.bookbom.shop.domain.booktag.repository;
+
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import shop.bookbom.shop.booktag.entity.BookTag;
+import shop.bookbom.shop.domain.booktag.entity.BookTag;
 
 public interface BookTagRepository extends JpaRepository<BookTag, Long> {
     // 책 id를 통해 BookTag의 리스트를 반환
