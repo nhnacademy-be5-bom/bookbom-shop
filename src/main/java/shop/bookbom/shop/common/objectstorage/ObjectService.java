5bcad38612d3c7b3d065907796c646be9ee58a60
hyeond0 <a98792530@gmail.com>
2024-04-11 23:21:03 +0900

    [feat] Object Storage 파일 다운로드 구현

--- src/main/java/shop/bookbom/shop/common/objectstorage/ObjectService.java
+++ src/main/java/shop/bookbom/shop/common/objectstorage/ObjectService.java
@@ -4,11 +4,17 @@ package shop.bookbom.shop.common.objectstorage;
 import java.io.IOException;
 import java.io.InputStream;
 import java.time.LocalDateTime;
+import java.util.Base64;
+import java.util.List;
 import lombok.NonNull;
 import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpMessageConverterExtractor;
@@ -20,15 +26,16 @@ import shop.bookbom.shop.common.objectstorage.exception.FileNotFoundException;
 
 @Service
 public class ObjectService {
-
+    private final RestTemplate restTemplate;
     private final TokenService tokenService;
     @Value("${object.storage.url}")
     private String storageUrl;
     private TokenInfo token;
 
     @Autowired
-    public ObjectService(TokenService tokenService) {
+    public ObjectService(TokenService tokenService, RestTemplate restTemplate) {
         this.tokenService = tokenService;
+        this.restTemplate = restTemplate;
         token = tokenService.requestToken();
     }
 
@@ -42,7 +49,7 @@ public class ObjectService {
      *
      * @param file          업로드할 파일 MultipartName
      * @param containerName 저장할 컨테이너 이름 ex) bookbom/book_thumbnail
-     * @param objectName    ex) 저장될 파일 이름
+     * @param objectName    저장될 파일 이름
      */
     public void uploadObject(MultipartFile file, String containerName, String objectName) {
         String url = this.getUrl(containerName, objectName);
@@ -57,12 +64,50 @@ public class ObjectService {
             };
             SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
             requestFactory.setBufferRequestBody(false);
-            RestTemplate restTemplate = new RestTemplate(requestFactory);
             HttpMessageConverterExtractor<String> responseExtractor
-                    = new HttpMessageConverterExtractor<>(String.class, restTemplate.getMessageConverters());
+                    = new HttpMessageConverterExtractor<>(String.class,
+                    new RestTemplate(requestFactory).getMessageConverters());
             restTemplate.execute(url, HttpMethod.PUT, requestCallback, responseExtractor);
         } catch (IOException e) {
             throw new FileNotFoundException();
         }
+    }
+
+    /**
+     * Object Storage에서 파일을 받아오는 메서드입니다.
+     *
+     * @param containerName 저장된 컨테이너 이름 ex) bookbom/book_thumbnail
+     * @param objectName    저장한 파일 이름
+     * @return 파일 데이터
+     */
+    public byte[] downloadObject(String containerName, String objectName) {
+        String url = this.getUrl(containerName, objectName);
+        if (token.getExpires().isBefore(LocalDateTime.now())) {
+            token = tokenService.requestToken();
+        }
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("X-Auth-Token", token.getId());
+        headers.setAccept(List.of(MediaType.APPLICATION_OCTET_STREAM));
+
+        HttpEntity<String> requestHttpEntity = new HttpEntity<>(null, headers);
+
+        ResponseEntity<byte[]> response = restTemplate.exchange(url, HttpMethod.GET, requestHttpEntity, byte[].class);
+
+        byte[] body = response.getBody();
+        if (body == null || body.length == 0) {
+            throw new FileNotFoundException();
+        }
+        return body;
+    }
+
+    /**
+     * Object Stroage에서 받아온 바이트 배열을 이미지 데이터로 바꿔주는 메서드입니다.
+     *
+     * @param buffer 이미지 데이터
+     * @return Base64로 인코딩한 이미지 데이터
+     */
+    public String bufferToUrl(byte[] buffer) {
+        String imageUrl = Base64.getEncoder().encodeToString(buffer);
+        return "data:image/jpeg;base64," + imageUrl;
     }
 }
