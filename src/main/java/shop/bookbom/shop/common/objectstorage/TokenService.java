7c9cf924318c43ec3c4127bb33d413da1ad2074d
hyeond0 <a98792530@gmail.com>
2024-04-11 23:31:14 +0900

    [fix] object storage 접속 정보 암호화 및 OSIV 설정

--- src/main/java/shop/bookbom/shop/common/objectstorage/TokenService.java
+++ src/main/java/shop/bookbom/shop/common/objectstorage/TokenService.java
@@ -4,7 +4,7 @@ import static shop.bookbom.shop.common.objectstorage.dto.TokenRequest.Auth;
 import static shop.bookbom.shop.common.objectstorage.dto.TokenRequest.PasswordCredentials;
 
 import java.util.Objects;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -15,10 +15,13 @@ import org.springframework.web.client.RestTemplate;
 import shop.bookbom.shop.common.objectstorage.dto.ObjectToken;
 import shop.bookbom.shop.common.objectstorage.dto.TokenInfo;
 import shop.bookbom.shop.common.objectstorage.dto.TokenRequest;
+import shop.bookbom.shop.config.SecureManager;
 
 @Service
+@RequiredArgsConstructor
 public class TokenService {
     private final RestTemplate restTemplate;
+    private final SecureManager secureManager;
     private TokenRequest tokenRequest;
     @Value("${object.storage.auth.url}")
     private String authUrl;
@@ -29,24 +32,22 @@ public class TokenService {
     @Value("${object.storage.password}")
     private String password;
 
-    @Autowired
-    public TokenService(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
-    }
-
 
     /**
      * Object Storage 파일 업로드를 위한 토큰을 저장하는 메서드입니다.
+     *
      * @return 생성된 Token 정보
      */
     public TokenInfo requestToken() {
         if (tokenRequest == null) {
-            tokenRequest = new TokenRequest(new Auth(tenantId, new PasswordCredentials(username, password)));
+            PasswordCredentials passwordCredentials = new PasswordCredentials(
+                    secureManager.getValue(username), secureManager.getValue(password));
+            tokenRequest = new TokenRequest(new Auth(tenantId, passwordCredentials));
         }
-        String identityUrl = this.authUrl + "/tokens";
+        String identityUrl = authUrl + "/tokens";
         HttpHeaders headers = new HttpHeaders();
         headers.add("Content-Type", "application/json");
-        HttpEntity<TokenRequest> httpEntity = new HttpEntity<>(this.tokenRequest, headers);
+        HttpEntity<TokenRequest> httpEntity = new HttpEntity<>(tokenRequest, headers);
         ResponseEntity<ObjectToken> response =
                 restTemplate.exchange(identityUrl, HttpMethod.POST, httpEntity, ObjectToken.class);
         return Objects.requireNonNull(response.getBody()).getAccess().getToken();
