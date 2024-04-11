cfd0c89dd10265029625aeec85d3c22603e02637
hyeond0 <a98792530@gmail.com>
2024-04-11 18:10:45 +0900

    [fix] Secure Key Manager 설정 로직 수정

--- src/main/java/shop/bookbom/shop/config/DataSourceConfig.java
+++ src/main/java/shop/bookbom/shop/config/DataSourceConfig.java
@@ -1,30 +1,18 @@
 package shop.bookbom.shop.config;
 
-import java.util.Objects;
 import javax.sql.DataSource;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.web.client.RestTemplate;
-import shop.bookbom.shop.config.dto.KeystoreResponse;
 
 @Configuration
-@Import({SecureManagerConfig.class})
 @RequiredArgsConstructor
 public class DataSourceConfig {
     private static final String MYSQL_URL_PREFIX = "jdbc:mysql://";
     private static final String MYSQL_DRIVER_NAME = "com.mysql.cj.jdbc.Driver";
-
-    private final RestTemplate restTemplate;
-
-    @Value("${keymanager.url}")
-    private String keyManagerUrl;
-
-    @Value("${secure.manager.appkey}")
-    private String appkey;
+    private final SecureManager secureManager;
 
 
     @Value("${secure.manager.ip}")
@@ -46,16 +34,14 @@ public class DataSourceConfig {
     @Bean
     public DataSource dataSource() {
         return DataSourceBuilder.create()
-                .url(MYSQL_URL_PREFIX + getValue(ip) + ":" + getValue(port) + getValue(schema))
-                .username(getValue(username))
-                .password(getValue(password))
+                .url(MYSQL_URL_PREFIX +
+                        secureManager.getValue(ip) +
+                        ":" +
+                        secureManager.getValue(port) +
+                        secureManager.getValue(schema))
+                .username(secureManager.getValue(username))
+                .password(secureManager.getValue(password))
                 .driverClassName(MYSQL_DRIVER_NAME)
                 .build();
-    }
-
-    private String getValue(String key) {
-        String url = keyManagerUrl + appkey + "/secrets/" + key;
-        KeystoreResponse response = restTemplate.getForObject(url, KeystoreResponse.class);
-        return Objects.requireNonNull(response).getBody().getSecret();
     }
 }
