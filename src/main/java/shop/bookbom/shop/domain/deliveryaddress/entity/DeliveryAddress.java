3411fbd802b8cc38866515498f2c5e74fda0cc8e
boyun <bu339@naver.com>
2024-04-15 14:10:17 +0900

    [fix] address 관련 엔티티 추가 및 수정

---
+++ src/main/java/shop/bookbom/shop/domain/deliveryaddress/entity/DeliveryAddress.java
@@ -0,0 +1,35 @@
+package shop.bookbom.shop.domain.deliveryaddress.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "delivery_address")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class DeliveryAddress {
+    @Id
+    private Long id;
+
+    @Column(name = "zip_code", nullable = false, length = 5)
+    private String zipCode;
+
+    @Column(name = "delivery_address", nullable = false, length = 200)
+    private String deliveryAddress;
+
+    @Column(name = "address_detail", nullable = false, length = 200)
+    private String addressDetail;
+
+    @Builder
+    public DeliveryAddress(String zipCode, String deliveryAddress, String addressDetail) {
+        this.zipCode = zipCode;
+        this.deliveryAddress = deliveryAddress;
+        this.addressDetail = addressDetail;
+    }
+}
