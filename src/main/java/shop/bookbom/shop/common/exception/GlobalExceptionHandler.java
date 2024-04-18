bb1b49e56877904e23f1662dcd5dcd0f76c2ee6c
bodol4748 <55184097+bodol4748@users.noreply.github.com>
2024-04-18 16:34:25 +0900

    [feat] validated exception handler 추가

--- src/main/java/shop/bookbom/shop/common/exception/GlobalExceptionHandler.java
+++ src/main/java/shop/bookbom/shop/common/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package shop.bookbom.shop.common.exception;
 
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import shop.bookbom.shop.common.CommonResponse;
@@ -10,5 +11,11 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(BaseException.class)
     public CommonResponse<Void> handleBaseException(BaseException e) {
         return CommonResponse.fail(e.getErrorCode());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public CommonResponse<Void> handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
+        // 사용자 정의 예외를 던질 수 있습니다.
+        return CommonResponse.fail(ErrorCode.COMMON_INVALID_PARAMETER);
     }
 }
