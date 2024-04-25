20a66b95f6d53f938f3b3110ede88b68e6e03f60
hyeond0 <a98792530@gmail.com>
2024-04-25 15:04:55 +0900

    [fix] TestEntityManager를 사용한 테스트 로직으로 수정

--- src/test/java/shop/bookbom/shop/domain/cart/repository/impl/CartRepositoryTest.java
+++ src/test/java/shop/bookbom/shop/domain/cart/repository/impl/CartRepositoryTest.java
@@ -14,71 +14,47 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.context.annotation.Import;
 import shop.bookbom.shop.config.TestQuerydslConfig;
 import shop.bookbom.shop.domain.book.entity.Book;
-import shop.bookbom.shop.domain.book.repository.BookRepository;
 import shop.bookbom.shop.domain.cart.entity.Cart;
 import shop.bookbom.shop.domain.cart.repository.CartRepository;
 import shop.bookbom.shop.domain.cartitem.entity.CartItem;
-import shop.bookbom.shop.domain.cartitem.repository.CartItemRepository;
 import shop.bookbom.shop.domain.member.entity.Member;
-import shop.bookbom.shop.domain.member.repository.MemberRepository;
 import shop.bookbom.shop.domain.pointrate.entity.PointRate;
-import shop.bookbom.shop.domain.pointrate.repository.PointRateRepository;
 import shop.bookbom.shop.domain.publisher.entity.Publisher;
-import shop.bookbom.shop.domain.publisher.repository.PublisherRepository;
 import shop.bookbom.shop.domain.rank.entity.Rank;
-import shop.bookbom.shop.domain.rank.repository.RankRepository;
 import shop.bookbom.shop.domain.role.entity.Role;
-import shop.bookbom.shop.domain.role.repository.RoleRepository;
 
 @Import(TestQuerydslConfig.class)
 @DataJpaTest
 class CartRepositoryTest {
 
     @Autowired
-    CartRepository cartRepository;
+    private TestEntityManager em;
 
     @Autowired
-    MemberRepository memberRepository;
-
-    @Autowired
-    BookRepository bookRepository;
-
-    @Autowired
-    RoleRepository roleRepository;
-
-    @Autowired
-    RankRepository rankRepository;
-
-    @Autowired
-    PointRateRepository pointRateRepository;
-
-    @Autowired
-    PublisherRepository publisherRepository;
-
-    @Autowired
-    CartItemRepository cartItemRepository;
+    private CartRepository cartRepository;
 
     @Test
     @DisplayName("장바구니 조회")
     void getCartWithCartItems() {
         // given
-        Role role = roleRepository.save(getRole());
-        PointRate pointrate = pointRateRepository.save(getPointRate());
-        Rank rank = rankRepository.save(getRank(pointrate));
-        Member member = memberRepository.save(getMember("test@email.com", role, rank));
-        Publisher publisher = publisherRepository.save(getPublisher());
-        Book book1 = bookRepository.save(getBook("title1", pointrate, publisher));
-        Book book2 = bookRepository.save(getBook("title2", pointrate, publisher));
+        Role role = em.persist(getRole());
+        PointRate pointrate = em.persist(getPointRate());
+        Rank rank = em.persist(getRank(pointrate));
+        Member member = em.persist(getMember("test@email.com", role, rank));
+        Publisher publisher = em.persist(getPublisher());
+        Book book1 = em.persist(getBook("title1", pointrate, publisher));
+        Book book2 = em.persist(getBook("title2", pointrate, publisher));
         Cart cart = getCart(member);
-        cartRepository.save(cart);
-        CartItem item1 = cartItemRepository.save(CartItem.builder()
+        em.persist(cart);
+        CartItem item1 = em.persist(CartItem.builder()
                 .cart(cart)
                 .book(book1)
                 .build());
-        CartItem item2 = cartItemRepository.save(CartItem.builder()
+        CartItem item2 = em.persist(CartItem.builder()
                 .cart(cart)
                 .book(book2)
                 .build());
