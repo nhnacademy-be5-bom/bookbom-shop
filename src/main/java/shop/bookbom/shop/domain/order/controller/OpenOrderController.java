40c26e1adbfd0b317b955994d4512c5ec45bb67a
boyun <bu339@naver.com>
2024-04-27 20:48:15 +0900

    [fix] order 비회원 controller 생성

---
+++ src/main/java/shop/bookbom/shop/domain/order/controller/OpenOrderController.java
@@ -0,0 +1,49 @@
+package shop.bookbom.shop.domain.order.controller;
+
+import javax.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import shop.bookbom.shop.common.CommonResponse;
+import shop.bookbom.shop.common.exception.ErrorCode;
+import shop.bookbom.shop.domain.order.dto.request.BeforeOrderRequestList;
+import shop.bookbom.shop.domain.order.dto.response.BeforeOrderResponse;
+import shop.bookbom.shop.domain.order.exception.OrderInfoInvalidException;
+import shop.bookbom.shop.domain.order.service.OrderService;
+
+@RestController
+@RequestMapping("/shop/open")
+@RequiredArgsConstructor
+public class OpenOrderController {
+    private final OrderService orderService;
+
+    /**
+     * 장바구니에서 넘어온 정보로 포장지 선택 페이지 구성을 위한 책 , 포장지 정보를 불러오는 api
+     *
+     * @param beforeOrderRequestList(책id와 수량의 리스트)
+     * @param bindingResult(for validation check)
+     * @return BeforeOrderResponse(총 주문 수, 포장지 전체 리스트, 책의 수량, 이미지, 가격, 제목)
+     */
+    @PostMapping("/orders/before-order")
+    public CommonResponse<?> beforeOrder(
+            @RequestBody @Valid BeforeOrderRequestList beforeOrderRequestList,
+            BindingResult bindingResult)
+            throws OrderInfoInvalidException {
+        //요청의 유효성 검사
+        if (bindingResult.hasErrors()) {
+            throw new OrderInfoInvalidException();
+        }
+        if (!orderService.checkStock(beforeOrderRequestList)) {
+            return CommonResponse.fail(ErrorCode.LOW_STOCK);
+        }
+        //요청에서 책 정보를 받아옴
+        BeforeOrderResponse beforeOrderResponse = orderService.getOrderBookInfo(beforeOrderRequestList);
+
+        //응답 반환
+        return CommonResponse.successWithData(beforeOrderResponse);
+    }
+
+}
