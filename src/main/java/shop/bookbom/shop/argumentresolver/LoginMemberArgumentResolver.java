35580d05965625d1c9d31ea37942c55a194ac7fc
hyeond0 <a98792530@gmail.com>
2024-05-10 16:13:05 +0900

    [fix] User 로그인 정보 쿠키 조회 공통화 처리

---
+++ src/main/java/shop/bookbom/shop/argumentresolver/LoginMemberArgumentResolver.java
@@ -0,0 +1,41 @@
+package shop.bookbom.shop.argumentresolver;
+
+import javax.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+import shop.bookbom.shop.annotation.Login;
+import shop.bookbom.shop.domain.users.dto.UserDto;
+import shop.bookbom.shop.domain.users.dto.response.UserIdRole;
+import shop.bookbom.shop.security.jwt.JwtConfig;
+
+@Slf4j
+@RequiredArgsConstructor
+public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolver {
+    private final JwtConfig jwtConfig;
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        boolean hasLoginAnnotation =
+                parameter.hasParameterAnnotation(Login.class);
+        boolean hasUserType =
+                UserDto.class.isAssignableFrom(parameter.getParameterType());
+        return hasLoginAnnotation && hasUserType;
+    }
+
+    @Override
+    public Object resolveArgument(
+            MethodParameter parameter,
+            ModelAndViewContainer mavContainer, NativeWebRequest webRequest,
+            WebDataBinderFactory binderFactory
+    ) throws Exception {
+        HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
+        String token = jwtConfig.resolveToken(request);
+        UserIdRole userIdRole = jwtConfig.getUserIdRole(token);
+        return new UserDto(userIdRole.getUserId());
+    }
+}
