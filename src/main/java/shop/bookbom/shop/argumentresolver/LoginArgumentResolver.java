d500ec34e5a942c95b1f904f9f1cc92c0394b38b
hyeond0 <a98792530@gmail.com>
2024-05-14 14:16:41 +0900

    [fix] LoginArgumentResolver에서 비회원일 경우 null을 반환하도록 수정

--- src/main/java/shop/bookbom/shop/argumentresolver/LoginArgumentResolver.java
+++ src/main/java/shop/bookbom/shop/argumentresolver/LoginArgumentResolver.java
@@ -1,22 +1,25 @@
 package shop.bookbom.shop.argumentresolver;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import javax.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 import shop.bookbom.shop.annotation.Login;
+import shop.bookbom.shop.config.JwtSecretKeyProperties;
 import shop.bookbom.shop.domain.users.dto.UserDto;
-import shop.bookbom.shop.domain.users.dto.response.UserIdRole;
-import shop.bookbom.shop.security.jwt.JwtConfig;
 
 @Slf4j
+@Component
 @RequiredArgsConstructor
 public class LoginArgumentResolver implements HandlerMethodArgumentResolver {
-    private final JwtConfig jwtConfig;
+    private final JwtSecretKeyProperties jwtSecretKeyProperties;
 
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
@@ -34,8 +37,21 @@ public class LoginArgumentResolver implements HandlerMethodArgumentResolver {
             WebDataBinderFactory binderFactory
     ) {
         HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
-        String token = jwtConfig.resolveToken(request);
-        UserIdRole userIdRole = jwtConfig.getUserIdRole(token);
-        return new UserDto(userIdRole.getUserId());
+        String authorization = request.getHeader("Authorization");
+        if (authorization == null || authorization.isBlank() || !authorization.contains("Bearer ")) {
+            return null;
+        }
+        String token = authorization.replace("Bearer ", "");
+        String secretKey = jwtSecretKeyProperties.getSecretKey();
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(secretKey.getBytes())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+        if (claims == null || claims.isEmpty()) {
+            return null;
+        }
+        Long userId = claims.get("userId", Long.class);
+        return new UserDto(userId);
     }
 }
